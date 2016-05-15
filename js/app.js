6433bda52d8a15f7c34cde1d6bd247aa6db2baf3
fizzy81 <fizzzzy81@gmail.com>
2016-05-16 00:00:39 +0200

    read files, parse json model and display json parsing errors in modal

--- js/app.js
+++ js/app.js
@@ -4,75 +4,173 @@ var app = angular.module('app', ['flow']);
 
 app.controller('main', function($scope, $sce) {
 
-  // modal stuff
+  // modal window object model
   $scope.modal = {
-    title: '',
+    title: 'Warning',
     content: []
   }
 
+  // triggered by the 'Close' button
   $scope.closeModal = function() {
+    // resets modal window properties
     $scope.modal = {
-      title: '',
+      title: 'Warning',
       content: []
-    }
+    };
   }
 
+  // errors model
+  $scope.errors = {
+    // internal error list
+    list: [],
+    // called when a new error occurs
+    push: function(string) {
+
+      // push the new error
+      this.list.push(string);
+
+      // override the modal content
+      $scope.modal.content = this.list.map(function(msg) {return $sce.trustAsHtml(msg)});
+
+    }
+  };
+
+  // loaded models filenames
   $scope.modelsList = [];
+  // loaded textures filenames
   $scope.texturesList = [];
 
-  // called when files are loaded
-  $scope.update = function($flow) {
+  // update global state when files are loaded
+  $scope.load = function($flow) {
 
+    // file array buffer
     var files = [];
-    var errors = [];
 
+    // result
     var models = {};
     var textures = {};
 
-    // filter models and textures
+    // set modal title just in case
+    $scope.modal.title = 'An error occured while loading resources.';
+    $scope.errors.list = [];
+
+    // only keep '.json' and '.png' extensions from $flow
     $flow.files.forEach(function(file, index) {
 
       var name = file.name;
       var len = name.length;
 
       if (name.toLowerCase().substr(len-5, len-1) == '.json') {
+        // register model
         models[name] = file;
         files.push(file);
       } else if (name.toLowerCase().substr(len-4, len-1) == '.png') {
+        // register texture
         textures[name] = file;
         files.push(file);
       } else {
-        errors.push('Can\'t load <strong>' + name + '</strong>, the file extension is invalid.')
+        // the filename extension is invalid
+        $scope.errors.push('Can\'t load <strong>' + name + '</strong>, the file extension is invalid.')
       }
 
     });
 
-    $scope.modelsList = Object.keys(models).sort(alphanum);
-    $scope.texturesList = Object.keys(textures).sort(alphanum);
+    // loaded files buffers
+    var modelsList = Object.keys(models).sort(alphanum);
+    var texturesList = Object.keys(textures).sort(alphanum);
 
-    $flow.files = files;
-
-    // display errors
-    if (errors.length > 0) {
-      var modal = {
-        title: 'An error occured while loading resources.',
-        content: []
-      };
-      errors.forEach(function(msg) {
-        modal.content.push($sce.trustAsHtml(msg));
-      });
-      $scope.modal = modal;
+    // the result after loading files
+    var loaded = {
+      models: [],
+      textures: []
     }
+
+    // load models
+    modelsList.forEach(function(modelName) {
+
+      // get the corresponding file
+      var model = models[modelName];
+      var file = model.file;
+
+      // prepare to read a file
+      var reader = new FileReader();
+
+      // when the reader is loaded
+      reader.onload = function(event) {
+        try {
+          // try to parse the json model and add it to the loaded variable
+          var result = JSON.parse(this.result);
+          loaded.models.push({
+            name: modelName,
+            data: result
+          });
+        } catch (e) {
+          // if there's an error, remove the file from the selection and it's name from the models list
+          $flow.files = $flow.files.filter(function(file) {
+            return file.name != modelName;
+          });
+          $scope.modelsList = $scope.modelsList.filter(function(name){
+            return name != modelName;
+          });
+          // if that's a SyntaxError then it's because of an error in the json
+          if (e instanceof SyntaxError) {
+            // push a new error with the details (which are browser specific)
+            $scope.errors.push('Can\'t load model <strong>' + modelName + '</strong>, json parsing failed. ' + e.message.trim().split(' in JSON ').join(' ') + '.');
+          } else {
+            // throw the exception if it's anything else
+            throw e;
+          }
+        }
+      }
+
+      // read the model
+      reader.readAsText(file);
+
+    });
+
+    // load textures
+    texturesList.forEach(function(textureName) {
+
+      // get the corresponding file
+      var texture = textures[textureName];
+      var file = texture.file;
+
+      // prepare to read a file
+      var reader = new FileReader();
+
+      // when the reader is loaded
+      reader.onload = function(event) {
+        // add the image to the loaded variable
+        loaded.textures.push({
+          name: textureName,
+          data: this.result
+        })
+      }
+
+      // read the image
+      reader.readAsDataURL(file);
+
+    });
+
+    console.log(loaded);
+
+    // update filenames list models
+    $scope.modelsList = modelsList;
+    $scope.texturesList = texturesList;
+
+    // update file selection
+    $flow.files = files;
 
   };
 
   // remove files
   $scope.remove = function(name, $flow) {
-    files = $flow.files.filter(function(file) {
+    // filter the file selection
+    $flow.files = $flow.files.filter(function(file) {
       return file.name != name;
     });
-    $flow.files = files;
-    $scope.update($flow);
+    // reload the view
+    $scope.load($flow);
   }
 
 });
